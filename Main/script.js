49d94e6d26c06be3b4b3080b97cddcecf6e043ea
Aleksandr Fadeev <333702@niuitmo.ru>
2024-11-12 18:57:59 +0300

    main optimized

--- Main/script.js
+++ Main/script.js
@@ -9,7 +9,7 @@ let currentPosition = 0;
 
 function moveToSlide(index) {
     const amountToMove = -itemWidth * index;
-    track.style.transform = `translate3D(${amountToMove}px, 0, 0)`;
+    track.style.transform = `translateX(${amountToMove}px)`;
     currentPosition = index;
 }
 
@@ -40,8 +40,8 @@ const itemWidth2 = items2[0].offsetWidth + 20;
 let currentPosition2 = 0;
 
 function moveCarousel(position, transition = true) {
-    merchTrack.style.transition = transition ? 'transform 0.7s ease' : 'none';
-    merchTrack.style.transform = `translate3D(${position}px, 0, 0)`;
+    merchTrack.style.transition = transition ? 'transform 0.7s ease-in-out' : 'none';
+    merchTrack.style.transform = `translateX(${position}px)`;
 }
 
 nextButton2.addEventListener('click', () => {
@@ -79,8 +79,8 @@ const itemWidth3 = items3[0].offsetWidth + 20;
 let currentPosition3 = 0;
 
 function moveCarousel2(position, transition = true) {
-    yaTrack.style.transition = transition ? 'transform 0.7s ease' : 'none';
-    yaTrack.style.transform = `translate3D(${position}px, 0, 0)`;
+    yaTrack.style.transition = transition ? 'transform 0.7s ease-in-out' : 'none';
+    yaTrack.style.transform = `translateX(${position}px)`;
 }
 
 nextButton3.addEventListener('click', () => {
@@ -119,8 +119,8 @@ const itemWidth4 = items4[0].offsetWidth + 20;
 let currentPosition4 = 0;
 
 function moveCarousel3(position, transition = true) {
-    paninTrack.style.transition = transition ? 'transform 0.7s ease' : 'none';
-    paninTrack.style.transform = `translate3D(${position}px, 0, 0)`;
+    paninTrack.style.transition = transition ? 'transform 0.7s ease-in-out' : 'none';
+    paninTrack.style.transform = `translateX(${position}px)`;
 }
 
 nextButton4.addEventListener('click', () => {
